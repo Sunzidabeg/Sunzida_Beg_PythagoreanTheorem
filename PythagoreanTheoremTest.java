54143d00787260a1153065c44b82bd40bc3d8d40
Sunzidabeg <50387582+Sunzidabeg@users.noreply.github.com>
2019-06-15 19:37:49 -0500

    Add files via upload

--- PythagoreanTheoremTest.java
+++ PythagoreanTheoremTest.java
@@ -6,7 +6,7 @@ public class PythagoreanTheoremTest {
    public static void main(String[] args) {
 PythagoreanTheorem calc = new PythagoreanTheorem();
 
-System.out.println(calc.CalcHyp(4, 3));    }
+System.out.println(calc.CalcHyp(20.1, 11.14));    }
 
 }
     
